09ee587c07c19f5fc0a645c1e386b586db7df4e6
Ashraf Chitambaa <98608195+Bsc-40-20@users.noreply.github.com>
2023-04-27 00:58:29 +0200

    Delete vitemail.module.d.ts

--- dist/vitemail.module.d.ts
+++
@@ -1,8 +0,0 @@
-import { DynamicModule } from '@nestjs/common';
-import { ConfigOptionsDto } from './dto/config-options.dto';
-export declare class ViteMailModule {
-    /** You need to register your account to vitemail */
-    static register(
-    /** Email and app password for google accounts */ options: ConfigOptionsDto): DynamicModule;
-}
-//# sourceMappingURL=vitemail.module.d.ts.map
