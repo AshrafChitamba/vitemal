ad65a1c9c70111713cf6352bb714eaba485c9793
Ashraf Chitambaa <98608195+Bsc-40-20@users.noreply.github.com>
2023-04-27 00:59:15 +0200

    Delete vitemail.service.d.ts

--- dist/vitemail.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { ConfigOptionsDto } from './dto/config-options.dto';
-import { EmailDto } from './dto/email.dto';
-export declare class ViteMailService {
-    private vitemailConfig;
-    constructor(vitemailConfig: ConfigOptionsDto);
-    /** The actual function which sends the email */
-    sendEmail(/** The email details you want to send */ emailDetails: EmailDto): unknown;
-}
-//# sourceMappingURL=vitemail.service.d.ts.map
